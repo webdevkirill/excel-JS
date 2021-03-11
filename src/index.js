59f5f8145d9f9b6a7430163fb38e4205cc3c75b3
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-03-11 14:20:45 +0300

    Добавляет store для работы с состоянием

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import './sass/index.sass';
+import {createStore} from './core/createStore';
+import {rootReducer} from './redux/rootReducer';
 import {Excel} from './components/excel/Excel';
 import {Header} from './components/header/Header';
 import {Toolbar} from './components/toolbar/Toolbar';
 import {Formula} from './components/formula/Formula';
 import {Table} from './components/table/Table';
 
+const store = createStore(rootReducer);
+
 const excel = new Excel('#app', {
     components: [Header, Toolbar, Formula, Table],
+    store
 });
 
 excel.render();
