9c4c854c159746bc2bfb30998a2362beee839ec1
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-03-11 14:54:48 +0300

    Добавляет action для изменения ширины колонок и диспатчит его в нужный момент

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import {Toolbar} from './components/toolbar/Toolbar';
 import {Formula} from './components/formula/Formula';
 import {Table} from './components/table/Table';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, {
+    colState: {}
+});
 
 const excel = new Excel('#app', {
     components: [Header, Toolbar, Formula, Table],
