6ecb96ed7fc72168275fd7b9364f0d159b77fc6b
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-03-12 15:32:38 +0300

    Добавляет базовый функционал по считыванию хеша для роутинга

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import './sass/index.sass';
-import {createStore} from './core/createStore';
-import {storage, debounce} from './core/utils';
-import {rootReducer} from './redux/rootReducer';
-import {initialState} from './redux/initialState';
-import {Excel} from './components/excel/Excel';
-import {Header} from './components/header/Header';
-import {Toolbar} from './components/toolbar/Toolbar';
-import {Formula} from './components/formula/Formula';
-import {Table} from './components/table/Table';
+// import {createStore} from './core/createStore';
+// import {storage, debounce} from './core/utils';
+// import {rootReducer} from './redux/rootReducer';
+// import {initialState} from './redux/initialState';
+// import {Excel} from './components/excel/Excel';
+// import {Header} from './components/header/Header';
+// import {Toolbar} from './components/toolbar/Toolbar';
+// import {Formula} from './components/formula/Formula';
+// import {Table} from './components/table/Table';
+import {Router} from './core/routes/Router';
 
-const store = createStore(rootReducer, initialState);
+// const store = createStore(rootReducer, initialState);
 
-const stateListener = debounce(state => storage('excel-state', state), 300);
+// const stateListener = debounce(state => storage('excel-state', state), 300);
 
-store.subscribe(stateListener);
+// store.subscribe(stateListener);
 
-const excel = new Excel('#app', {
-    components: [Header, Toolbar, Formula, Table],
-    store
-});
+// const excel = new Excel('#app', {
+//     components: [Header, Toolbar, Formula, Table],
+//     store
+// });
 
-excel.render();
+// excel.render();
+
+const router = new Router('#app', {});
